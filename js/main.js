f4e699da10d47cafa3deb2b8b1c498ccc5aeef44
Nick <108188470+nickhallden1330@users.noreply.github.com>
2022-10-08 10:38:09 +0500

    add theme

--- js/main.js
+++ js/main.js
@@ -3,26 +3,100 @@ let elInp = document.querySelector('.js-inp');
 let elInp1 = document.querySelector('.js-inp1');
 let elInp2 = document.querySelector('.js-inp2');
 let elForm = document.querySelector('.js-form');
+let elBtn = document.querySelector('.js-btn');
+var theme = false;
 
-elForm.addEventListener('submit', function (evt) {
+const localData = JSON.parse(window.localStorage.getItem('List'));
+
+let users = localData || [];
+let sortNum = [];
+
+const newFunc = (arr, node) => {
+	node.innerHTML = '';
+	arr.forEach((el) => {
+		let elItem = document.createElement('li');
+		let elTitle = document.createElement('h3');
+		let elText = document.createElement('p');
+		let elLink = document.createElement('a');
+		let elBtn = document.createElement('button');
+
+		elTitle.textContent = el.name;
+		elText.textContent = el.relationship;
+		elLink.textContent = el.phone_number;
+		elLink.href = `tel:${el.phone_number}`;
+		elLink.setAttribute('class', 'btn btn-info');
+		elBtn.textContent = 'Delete';
+		elBtn.type = 'submit';
+		elBtn.setAttribute('class', 'btn btn-danger ms-4 js-btn');
+		elBtn.dataset.userId = el.id;
+
+		elInp.value = '';
+		elInp1.value = '';
+		elInp2.value = '';
+
+		elItem.appendChild(elTitle);
+		elItem.appendChild(elText);
+		elItem.appendChild(elLink);
+		elItem.appendChild(elBtn);
+		node.appendChild(elItem);
+	});
+};
+
+newFunc(users, elList);
+
+elForm.addEventListener('submit', (evt) => {
 	evt.preventDefault();
+	let val1 = elInp.value;
+	let val2 = elInp1.value;
+	let val3 = elInp2.value;
 
-	let elItem = document.createElement('li');
-	let elTitle = document.createElement('h3');
-	let elText = document.createElement('p');
-	let elLink = document.createElement('a');
+	let finded = users.findIndex((item) => item.phone_number == val3);
 
-	elTitle.textContent = elInp.value;
-	elText.textContent = elInp1.value;
-	elLink.textContent = elInp2.value;
-	elLink.href = `tel:${elInp2.value}`;
+	if (finded >= 0) {
+		alert('Простите этот номер уже зарегистрирован');
+	} else {
+		elInp.value = '';
+		elInp1.value = '';
+		elInp2.value = '';
 
-	elInp.value = '';
-	elInp1.value = '';
-	elInp2.value = '';
+		users.push({
+			id: users.length + 1,
+			name: val1,
+			relationship: val2,
+			phone_number: val3,
+		});
 
-	elItem.appendChild(elTitle);
-	elItem.appendChild(elText);
-	elItem.appendChild(elLink);
-	elList.appendChild(elItem);
+		newFunc(users, elList);
+
+		sortNum.push(val3);
+	}
+	window.localStorage.setItem('List', JSON.stringify(users));
 });
+
+elList.addEventListener('click', function (evt) {
+	if (evt.target.matches('.js-btn')) {
+		let userId = evt.target.dataset.userId;
+
+		let delUser = users.findIndex((index) => index.id == userId);
+
+		users.splice(delUser, 1);
+		newFunc(users, elList);
+		window.localStorage.setItem('List', JSON.stringify(users));
+	}
+});
+
+elBtn.addEventListener('click' , () => {
+	theme = !theme
+	window.localStorage.setItem('theme' , theme ? 'dark' : 'light')
+	changeTheme()
+})
+
+function changeTheme() {
+	if(window.localStorage.getItem('theme') == 'dark') {
+		document.body.classList.add('dark')
+	}
+	else{
+		document.body.classList.remove('dark')
+	}
+}
+changeTheme()
